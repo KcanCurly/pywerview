c4615bb24fe2a54ef363841bace771341ceb5b98
Kerim Can Karaduman <96576479+KcanCurly@users.noreply.github.com>
2024-11-27 21:39:45 +0300

    fix 3

--- setup.py
+++ setup.py
@@ -17,7 +17,8 @@ setup(
         'Topic :: Security',
     ],
     requires=[
-        "git+https://github.com/fortra/impacket@db71504529008bdbdd900549f6e4293be2e32c88",
+        "impacket @ git+https://github.com/fortra/impacket@db71504529008bdbdd900549f6e4293be2e32c88#egg=impacket",
+
         "beautifulsoup4",
         "lxml",
         "pyasn1",
